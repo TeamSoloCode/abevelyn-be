02679f3bd7fc8d010745abbde712f285df5af88d
BruceBui5041 <brucebui5041@gmail.com>
2021-11-06 14:45:25 +0700

    update init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CollectionsModule } from './collections/collections.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CurrencyModule } from './currency/currency.module';
+import { ColorsModule } from './colors/colors.module';
+import { FeedbacksModule } from './feedbacks/feedbacks.module';
+import { CartsModule } from './carts/carts.module';
+import { CouponsModule } from './coupons/coupons.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderStatusModule } from './order-status/order-status.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [CollectionsModule],
+  imports: [CollectionsModule, ProductsModule, UsersModule, AuthModule, CurrencyModule, ColorsModule, FeedbacksModule, CartsModule, CouponsModule, OrdersModule, OrderStatusModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
