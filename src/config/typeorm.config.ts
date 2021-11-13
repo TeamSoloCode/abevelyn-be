abd406c371144515eb5d4ae029eb8bf4fff64b1f
longnh14 <chugaconchaylonton@gmail.com>
2021-11-13 19:25:58 +0700

    change pwd db

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'Dr@g0n12021996',
+  password: 'root',
   database: 'abevelyn_dev',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: ['migration/*.js'],
