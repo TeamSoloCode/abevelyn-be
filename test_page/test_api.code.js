995dcc850900c00579a87b2c43794951c9c60c7d
BruceBui5041 <brucebui5041@gmail.com>
2022-06-21 23:55:32 +0700

    Update test_api page

--- test_page/test_api.code.js
+++ test_page/test_api.code.js
@@ -32,8 +32,22 @@ const fetchSuccess = React.useCallback(async () => {
   }
 }, [setPageData]);
 
+const genTestURL = async (uuid) => {
+  return fetchData(`http://localhost:3000/products/${uuid}`);
+};
+
+const multiFetch = async () => {
+  const abcd = await Promise.all([
+    genTestURL('0156f421-d727-41c7-b60e-50c3e3b82613'),
+    genTestURL('0d061d92-a73c-456e-a352-bdc1d9f65840'),
+    genTestURL('219d94c2-f045-4a89-9648-d9cb75bb77f1'),
+  ]);
+  console.log('abcd multiFetch', abcd);
+};
+
 exportPageContext({
   invalidURL,
   fetchErrorFromBE,
   fetchSuccess,
+  multiFetch,
 });
