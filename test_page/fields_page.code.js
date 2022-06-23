43c78a3f76937f759ade78fa6b0b813ec5604590
BruceBui5041 <brucebui5041@gmail.com>
2022-06-23 09:58:50 +0700

    Use useInitState in layout code

--- test_page/fields_page.code.js
+++ test_page/fields_page.code.js
@@ -1,3 +1,5 @@
+useInitState({ textFieldValue: 'init text value' });
+
 React.useEffect(() => {
   if (!_.isEqual(pageData?.data, prevPageData?.data)) {
     setPageData({ data: JSON.stringify(pageData) });
@@ -8,7 +10,7 @@ React.useEffect(() => {}, [pageData?.textFieldValue]);
 
 exportPageContext({
   onTextChange: (value) => {
-    setPageData({ textValue: value });
+    setPageData({ textFieldValue: value });
   },
   resetAllText: () => {
     setPageData({ textFieldValue: '' });
